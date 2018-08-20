95efa32b63fe683e768551f07c44f8a62f6287cc
bohlmann <#TCPWJon1707>
2018-08-20 13:35:43 +0200

    Modify model interface strucutre

---
+++ WebAPI/Core/Interfaces/IModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Core.Interfaces
+{
+    public interface IModel<M> where M : BaseModel
+    {
+        void Create(M model);
+        void Delete(M model);
+        void Update(M model);
+        M Get(int id);
+        List<M> Get();
+    }
+}
--- WebAPI/Models/User.cs
+++ WebAPI/Models/User.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Core;
+using WebAPI.Core.Interfaces;
 
 namespace WebAPI.Models
 {
-    public class User
+    public class User : BaseModel, IModel<User>
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public void Create(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
