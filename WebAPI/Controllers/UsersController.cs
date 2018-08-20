b87023a4dd59e3e74aee50ad1da838e0245f4079
drdenni <dennis.kuerschner1996@gmail.com>
2018-08-20 13:18:14 +0200

    Add IUserModel Interface

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        
+
+
+        public List<User> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ WebAPI/Models/IUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public interface IUserModel
+    {
+        void CreateUser(User user);
+        void DeleteUser(User user);
+        void UpdateUser(User user);
+        User GetUser(int id);
+        List<User> GetUser();
+    }
+}
